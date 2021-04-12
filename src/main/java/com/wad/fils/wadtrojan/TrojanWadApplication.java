25560a954a9dcc99b41ecbdd6a4b4dac7693b930
ClickFlick <omerfarkozkul@gmail.com>
2021-04-12 10:58:50 +0300

    Initial Commit

---
+++ src/main/java/com/wad/fils/wadtrojan/TrojanWadApplication.java
@@ -0,0 +1,13 @@
+package com.wad.fils.wadtrojan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrojanWadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrojanWadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wad/fils/wadtrojan/TrojanWadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wad.fils.wadtrojan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrojanWadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
